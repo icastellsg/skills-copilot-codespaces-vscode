222310568fba699b3c2e3a438bbf43895a4d29ad
Iván Castells Galindo <72543054+icastellsg@users.noreply.github.com>
2023-11-20 11:00:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberName = document.getElementById("memberName");
+    var memberEmail = document.getElementById("memberEmail");
+    var memberPhone = document.getElementById("memberPhone");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsList = document.getElementById("memberSkillsList");
+    var memberSkillsList2 = document.getElementById("memberSkillsList2");
+    var memberSkillsList3 = document.getElementById("memberSkillsList3");
+    var memberSkillsList4 = document.getElementById("memberSkillsList4");
+    var memberSkillsList5 = document.getElementById("memberSkillsList5");
+    var memberSkillsList6 = document.getElementById("memberSkillsList6");
+    var memberSkillsList7 = document.getElementById("memberSkillsList7");
+    var memberSkillsList8 = document.getElementById("memberSkillsList8");
+    var memberSkillsList9 = document.getElementById("memberSkillsList9");
+    var memberSkillsList10 = document.getElementById("memberSkillsList10");
+}
